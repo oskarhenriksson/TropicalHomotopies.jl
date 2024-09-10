89c5f842d53057727ca43b369a0c8d444e1953e4
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2024-09-10 10:31:01 -0500

    Remove old files

--- old/TropicalHomotopies.jl
+++
@@ -1,9 +0,0 @@
-using Oscar
-import HomotopyContinuation as HC
-using LinearAlgebra
-
-
-include("Homotopies.jl")
-include("RootCounts.jl")
-include("Specialization.jl")
-include("VerticallyParametrizedSystems.jl")
